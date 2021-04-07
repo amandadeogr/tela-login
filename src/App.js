56721f76d334c499da3539a775509ed540d67ee7
amandadeogr <amandadeog@gmail.com>
2021-04-06 22:40:05 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Entrar</h3>
+
+      <div className="Entradas">
+        <input placeholder="Digite seu email" id="email"></input>
+        <input placeholder="Digite sua senha" id="email"></input>
+      </div>
+
+      <br/>
+      <button type="submit">Entrar</button>
+      <a className="Link">Esqueci minha senha</a>
+
+      <div className="caixa-redes-sociais">
+        <button type="submit" className="Redes-sociais"><i class="fab fa-google"></i> Entrar com o Google</button>
+        <button type="submit" className="Redes-sociais"><i class="fab fa-facebook"></i> Entrar com o Facebook</button>
+      </div>
     </div>
+
   );
 }
 
